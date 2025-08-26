35627f44b0099cb3e0e3287060edb4e474330b1f
Documenter.jl <documenter@juliadocs.github.io>
2025-08-26 09:19:22 +0000

    build based on ca8971b

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MemberFunctions","category":"page"},{"location":"#MemberFunctions","page":"Home","title":"MemberFunctions","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MemberFunctions.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MemberFunctions]","category":"page"}]
+[{"location":"#MemberFunctions","page":"Home","title":"MemberFunctions","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MemberFunctions.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
 }
